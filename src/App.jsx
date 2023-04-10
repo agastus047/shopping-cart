caaac07d2830f85244d6237750cf46787d5bbd57
Agastus Joyson <agastusjoyson@gmail.com>
2023-04-10 18:33:51 +0530

    Set state updation logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,48 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Shop from './components/Shop'
 import Cart from './components/Cart'
+import itemsList from './assets/itemsList'
+import { useState } from 'react'
 
 function App() {
+  const [items,setItems] = useState(itemsList);
+  const [cartContents,setCartContents] = useState([]);
+
+  function handleAddToCart(id) {
+    let selectedItem;
+    if(cartContents.some(item => item.id === id)) {
+      setCartContents(cartContents.map(item => {
+        if(item.id === id) {
+          return {...item,count:count+1};
+        }
+        return item;
+      }));
+    }
+    else {
+      selectedItem = items.find(item => item.id === id);
+      selectedItem = {...selectedItem,count: 1 };
+      setCartContents([...cartContents,selectedItem]);
+    }
+  }
+
+  function handleDecremetCount(id) {
+    let selectedItem = cartContents.find(item => item.id === id);
+    if(selectedItem.count === 1) {
+      handleDeleteFromCart(id);
+    }
+    else {
+      setCartContents(cartContents.map(item => {
+        if(item.id === id) {
+          return {...item,count:count-1};
+        }
+        return item;
+      }));
+    }
+  }
+
+  function handleDeleteFromCart(id) {
+    setCartContents(cartContents.filter(item => item.id !== id));
+  }
 
   return (
     <div className="App">
@@ -13,8 +53,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="/shop" element={<Shop/>} />
-          <Route path="/cart" element={<Cart/>} />
+          <Route path="/shop" element={<Shop handleAddToCart={handleAddToCart}/>} />
+          <Route path="/cart" element={<Cart handleAddToCart={handleAddToCart} handleDeleteFromCart={handleDeleteFromCart} handleDecremetCount={handleDecremetCount} />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/assets/itemsList.js
@@ -0,0 +1,29 @@
+const itemsList = [
+    {
+        id:0,
+        title:"item 1",
+        price:100
+    },
+    {
+        id:1,
+        title:"item 2",
+        price:200
+    },
+    {
+        id:2,
+        title:"item 3",
+        price:300
+    },
+    {
+        id:3,
+        title:"item 4",
+        price:400
+    },
+    {
+        id:4,
+        title:"item 5",
+        price:500
+    },
+];
+
+export default itemsList;
