03bcad2bc19ef8ff82e567ff59b98d65505b478a
Agastus Joyson <agastusjoyson@gmail.com>
2023-04-15 11:26:20 +0530

    Style product page

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -11,10 +11,12 @@ const ItemCard = ({items,handleAddToCart}) => {
     const selectedItem = items.find(item => item.id === itemId);
     
     return(
-        <div>
-            <div>{selectedItem.title}</div>
-            <div>{selectedItem.price}</div>
-            <button onClick={handleClick}>Add to cart</button>
+        <div className="flex-auto bg-[rgb(245,245,245)] p-7 sm:px-24 flex flex-col items-center">
+            <img src={selectedItem.image} alt="shoe_image" className="h-56 sm:h-72"></img>
+            <div className="font-extrabold text-xl">{selectedItem.title}</div>
+            <div className="text-lg py-3">MRP: ₹{selectedItem.price}</div>
+            <div className="text-slate-600 pb-3">{selectedItem.description}</div>
+            <button className="btn btn-border-4" onClick={handleClick}>Add to Cart</button>
         </div>
     );
 };
