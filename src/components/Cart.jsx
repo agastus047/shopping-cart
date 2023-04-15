aa19018f08402d70ac926211670e8d27b6ed72b7
Agastus Joyson <agastusjoyson@gmail.com>
2023-04-15 19:24:22 +0530

    Style cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -11,12 +11,20 @@ const Cart = ({cartContents,handleAddToCart,handleDeleteFromCart,handleDecrement
     },0);
 
     return(
-        <div>
-            {renderedCards}
-            <div>
-                <div>Total Amount: {totalSum}</div>
-                <button>CHECKOUT</button>
-            </div>
+        <div className="flex-auto flex flex-col p-7 sm:p-20 items-center sm:grid grid-cols-[2fr_1fr] sm:gap-20 sm:items-start">
+            {cartContents.length>0 ?
+                <div className="bg-neutral-100 p-3 rounded-xl">{renderedCards}</div> :
+                <div className="text-4xl text-center mt-20 font-bold text-gray-400 sm:text-end">Your cart is empty</div>
+            }
+            {cartContents.length>0 &&
+            <div className="mt-10 p-6 bg-neutral-100 rounded-xl w-full">
+                <div className="flex justify-between mb-2"><span>Subtotal: </span><span>₹{totalSum}</span></div>
+                <hr className="border-neutral-300"></hr>
+                <div className="flex justify-between my-2"><span>Delivery: </span><span>Free</span></div>
+                <hr className="border-neutral-300"></hr>
+                <div className="flex justify-between my-2"><span>Total: </span><span>₹{totalSum}</span></div>
+                <div className="flex justify-center mt-3"><button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">CHECKOUT</button></div>
+            </div>}
         </div>
     );
 };
--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -13,14 +13,19 @@ const CartCard = ({item,handleAddToCart,handleDeleteFromCart,handleDecrementCoun
     };
 
     return(
-        <div>
-            <div>{item.title}</div>
-            <div>{item.price}</div>
-            <div>{item.count}</div>
-            <div>Total Amount: {item.price*item.count}</div>
-            <button onClick={handleAddClick}>...+...</button>
-            <button onClick={handleDecrementClick}>...-...</button>
-            <button onClick={handleDeleteClick}>remove</button>
+        <div className=" w-full grid grid-cols-[1fr_2fr] mt-3 sm:mt-0 sm:pl-14 ">
+            <img src={item.image} alt='shoe_image'></img>
+            <div className="pl-3 self-center">
+                <div className="font-bold sm:text-lg">{item.title}</div>
+                
+                <div>Total: ₹{item.price*item.count}</div>
+                <div className="flex gap-1">
+                    <button className="hover:bg-slate-300 text-xs p-1 border border-neutral-600 rounded-[50%] flex justify-center items-center" onClick={handleAddClick}><span className="pi pi-plus"></span></button>
+                    <div>{item.count}</div>
+                    <button className="hover:bg-slate-300 text-xs p-1 border border-neutral-600 rounded-[50%] flex justify-center items-center" onClick={handleDecrementClick}><span className="pi pi-minus"></span></button>
+                    <button className="hover:bg-slate-300 ml-auto sm:ml-32 text-xs p-1 border border-neutral-600 rounded-[50%] flex justify-center items-center" onClick={handleDeleteClick}><span className="pi pi-trash"></span></button>
+                </div>
+            </div>
         </div>
     );
 };
